94dd32dfc065b3408fee41ba7c3361501ab4af04
lex <lex@users.noreply.github.com>
2017-11-22 15:25:27 +0200

    add hello world

--- src/main/java/com/example/fakenews/DemoApplication.java
+++ src/main/java/com/example/fakenews/DemoApplication.java
@@ -2,11 +2,20 @@ package com.example.fakenews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
