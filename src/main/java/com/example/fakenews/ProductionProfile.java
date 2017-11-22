2e49d1a154fdecc36e1a40552f08581a29f8577f
lex <lex@users.noreply.github.com>
2017-11-22 16:06:05 +0200

    add database?

---
+++ src/main/java/com/example/fakenews/ProductionProfile.java
@@ -0,0 +1,20 @@
+package com.example.fakenews;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("production")
+public class ProductionProfile {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
