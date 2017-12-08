b93adcbe0a98c2fb86dd1dd0b39c09ff8b49f970
lex <lex@users.noreply.github.com>
2017-12-08 18:46:53 +0200

    add models

---
+++ src/main/java/pw/yuuh/genuinenews/domain/Author.java
@@ -0,0 +1,22 @@
+package pw.yuuh.genuinenews.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Author extends AbstractPersistable<Long> {
+    @NotNull
+    @Column(unique=true)
+    @Size(min = 2, max = 100)
+    private String name;
+}
---
+++ src/main/java/pw/yuuh/genuinenews/domain/NewsItem.java
@@ -0,0 +1,49 @@
+package pw.yuuh.genuinenews.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class NewsItem extends AbstractPersistable<Long> {
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String title;
+
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String lead;
+
+    @NotNull
+    @Size(min = 5, max = 10000)
+    private String image;
+
+    @NotNull
+    @Size(min = 5, max = 10000)
+    private String content;
+
+    @NotNull
+    private long timesRead;
+
+    @NotNull
+    private LocalDateTime publishedAt;
+
+    @NotNull
+    @ManyToMany
+    private List<Category> categories;
+
+    @NotNull
+    @ManyToMany
+    private List<Author> authors;
+}
